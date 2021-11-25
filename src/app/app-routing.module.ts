db6b60fd8195e9762a9b2625ac94807de94fcc36
Richard Gualancañay <51199793+RichardG20105@users.noreply.github.com>
2021-11-25 12:52:44 -0500

    CSS Cambios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DocenteComponent } from './components/docente/docente.component';
 import { EstudianteComponent } from './components/estudiante/estudiante.component';
+import { ProyectoComponent } from './components/proyecto/proyecto.component';
 
 const routes: Routes = [
-  { path: 'estudiante', component: EstudianteComponent }
+  { path: '', component: ProyectoComponent},
+  { path: 'estudiante', component: EstudianteComponent },
+  { path: 'docente', component: DocenteComponent},
+  { path: 'proyecto', component: ProyectoComponent}
 ];
 
 @NgModule({
